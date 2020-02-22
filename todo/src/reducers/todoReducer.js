b1eeafb076fc9f88d6de391a6ffec1f6cdf0a900
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-02-21 19:44:37 -0800

    built out reducer function for step one

---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,26 @@
+export const initialState = {
+    todo: [
+        {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+        }
+    ]
+};
+
+export const todoReducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            const newTodo = {
+                item: action.payload,
+                completed: false,
+                id: Date.now()
+            };
+            return {
+                ...state,
+                todo: [{...state.todo}, newTodo]
+            };
+            default: 
+                return state;
+    }
+};
